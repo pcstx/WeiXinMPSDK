4a3634f56c0824f41e3bbf5cf47f91f8c00c9e5c
pcstx1 <pcstx1@163.com>
2016-01-25 17:50:32 +0800

    增加字段

--- Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/Analysis/AnalysisResultJson/ArticleAnalysisItemJson.cs
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/Analysis/AnalysisResultJson/ArticleAnalysisItemJson.cs
@@ -190,6 +190,8 @@ namespace Senparc.Weixin.MP.AdvancedAPIs.Analysis
         /// 数据的小时，包括从000到2300，分别代表的是[000,100)到[2300,2400)，即每日的第1小时和最后1小时
         /// </summary>
         public int ref_hour { get; set; }
+
+        public int user_source { get; set; }
         /// <summary>
         /// 图文页（点击群发图文卡片进入的页面）的阅读人数
         /// </summary>
