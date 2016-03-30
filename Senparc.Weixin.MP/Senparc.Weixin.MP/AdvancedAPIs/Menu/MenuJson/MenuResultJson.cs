1440a84378a3bc9a374e9f939ca24d92564881df
pcstx <pcstx@163.com>
2016-03-30 15:13:13 +0800

    获取自定义菜单配置接口实体类修改

--- Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/Menu/MenuJson/MenuResultJson.cs
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/Menu/MenuJson/MenuResultJson.cs
@@ -17,7 +17,56 @@ namespace Senparc.Weixin.MP.AdvancedAPIs.Menu
 
     public class SelfMenuInfo
     {
-        public List<MenuFull_RootButton> button { get; set; }
+        public List<Button> button { get; set; }
+    }
+
+    public class Button
+    {
+        public string name { get; set; }
+        public string type { get; set; } 
+        public string value { get; set; }
+        public SubButton sub_button { get; set; }
+    }
+
+    public class SubButton
+    {
+        public List<List> list { get; set; }
+    }
+
+    public class List
+    {
+        public string type { get; set; }
+
+        public string name { get; set; }
+
+        public string url { get; set; }
+
+        public NewsInfo news_info { get; set; }
+
+        public string value { get; set; }
+    }
+
+    public class NewsInfo
+    {
+        public List<NewsInfoList> list { get; set; }
+    }
+
+    public class NewsInfoList
+    {
+        public string title { get; set; }
+
+        public string author { get; set; }
+
+        public string digest { get; set; }
+
+        public int show_cover { get; set; }
+
+        public string cover_url { get; set; }
+
+        public string content_url { get; set; }
+
+        public string source_url { get; set; }
+
     }
 
 }
